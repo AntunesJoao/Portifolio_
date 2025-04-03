200a81518f7a51d76e8ffac163d3cf894c2e5656
AntunesJoão <antunesjoao81@gmail.com>
2025-04-03 00:17:29 -0400

    home

---
+++ portifolio/src/routes/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about-control'>
+      <h1>Resumo</h1>
+      <p> </p>
+    </div>
+  )
+}
+
+export default About;
